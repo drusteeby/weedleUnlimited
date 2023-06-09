108eb6990d0984131ec04309195b7cee5f289310
drusteeby <drusteeby@gmail.com>
2023-06-08 20:17:16 -0400

    convert  AutoSelect to functional component add onGuessSubmitted remove enter as submission method

--- weedleunlimited/src/App.js
+++ weedleunlimited/src/App.js
@@ -5,16 +5,12 @@ import pokedex from './Data/pokedex.json'
 import GuessTable from './Components/GuessTable';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function onChangeName(e){
-  console.log(e);
-};
-
 function App() {
 
   let pokeNames = pokedex.map((entry) => entry.name.english);
   let correctAnswer = pokedex.find(entry => entry.name.english === "Weedle");
 
-  console.log(correctAnswer);
+  let onGuessSubmitted = (guess) => console.log(guess);
 
   return (
 <div className="App">
@@ -22,9 +18,9 @@ function App() {
     <h1 className="App-header"> Weedle Unlimited</h1>
     <h2 className="App-caption">WEEDLE GUESSING GAME</h2>
     <GuessTable guesses={pokedex} correctAnswer={correctAnswer}/>
-    <input  onChange={onChangeName}></input>
     <AutoSelect
         options={pokeNames}
+        onGuessSubmitted={onGuessSubmitted}
       />
 </div>
   );
--- weedleunlimited/src/Components/AutoSelect.jsx
+++ weedleunlimited/src/Components/AutoSelect.jsx
@@ -1,67 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export class Autocomplete extends Component
+
+let propTypes = {
+    options: PropTypes.instanceOf(Array).isRequired
+};
+
+
+const AutoSelect = ({ options, onGuessSubmitted }) =>
 {
-    static propTypes = {
-        options: PropTypes.instanceOf(Array).isRequired
-    };
-    state = {
-        activeOption: 0,
-        filteredOptions: [],
-        showOptions: false,
-        userInput: ''
-    };
+    const [activeOption, setActiveOption] = useState(0);
+    const [filteredOptions, setFilteredOptions] = useState([]);
+    const [showOptions, setShowOptions] = useState(false);
+    const [userInput, setUserInput] = useState('');
 
-    onChange = (e) =>
+    let filterAnswerBeforeSubmit = (answer) =>
     {
-        console.log('onChanges');
-
-        const { options } = this.props;
-        const userInput = e.currentTarget.value;
-
-        const filteredOptions = options.filter(
-            (optionName) =>
-                optionName.toLowerCase().indexOf(userInput.toLowerCase()) > -1
-        );
-
-        this.setState({
-            activeOption: 0,
-            filteredOptions,
-            showOptions: true,
-            userInput: e.currentTarget.value
-        });
-    };
-
-    onClick = (e) =>
-    {
-        this.setState({
-            activeOption: 0,
-            filteredOptions: [],
-            showOptions: false,
-            userInput: e.currentTarget.innerText
-        });
-    };
-    onKeyDown = (e) =>
-    {
-        const { activeOption, filteredOptions } = this.state;
-
-        if (e.keyCode === 13)
+        if (answer.length > 0)
         {
-            this.setState({
-                activeOption: 0,
-                showOptions: false,
-                userInput: filteredOptions[activeOption]
-            });
-        } 
-        else if (e.keyCode === 38)
+            onGuessSubmitted(answer);
+        }
+    }
+
+    function onClick(e)
+    {
+        setActiveOption(0);
+        setFilteredOptions([]);
+        setShowOptions(false);
+        filterAnswerBeforeSubmit(e.currentTarget.innerText);
+        setUserInput('');
+    };
+
+    function onKeyDown(e)
+    {
+        if (e.keyCode === 38)
         {
             if (activeOption === 0)
             {
                 return;
             }
-            this.setState({ activeOption: activeOption - 1 });
-        } 
+            setActiveOption(activeOption - 1);
+        }
         else if (e.keyCode === 40)
         {
             if (activeOption === filteredOptions.length - 1)
@@ -69,63 +48,67 @@ export class Autocomplete extends Component
                 console.log(activeOption);
                 return;
             }
-            this.setState({ activeOption: activeOption + 1 });
+            setActiveOption(activeOption + 1);
         }
     };
 
-    render()
+    function onChange(e)
     {
-        const {
-            onChange,
-            onClick,
-            onKeyDown,
+        const userInput = e.currentTarget.value;
 
-            state: { activeOption, filteredOptions, showOptions, userInput }
-        } = this;
-        let optionList;
-        if (showOptions && userInput)
-        {
-            if (filteredOptions.length)
-            {
-                optionList = (
-                    <ul className="options">
-                        {filteredOptions.map((optionName, index) =>
-                        {
-                            let className = (index === activeOption) ? 'option-active' : '';
-                            return (
-                                <li className={className} key={optionName} onClick={onClick}>
-                                    {optionName}
-                                </li>
-                            );
-                        })}
-                    </ul>
-                );
-            } 
-            else
-            {
-                optionList = (
-                    <div className="no-options">
-                        <em>No Option!</em>
-                    </div>
-                );
-            }
-        }
-        return (
-            <React.Fragment>
-                <div className="search">
-                    <input
-                        type="text"
-                        className="search-box"
-                        onChange={onChange}
-                        onKeyDown={onKeyDown}
-                        value={userInput}
-                    />
-                    <input type="submit" value="" className="search-btn" />
-                </div>
-                {optionList}
-            </React.Fragment>
+        const filteredOptions = options.filter(
+            (optionName) =>
+                optionName.toLowerCase().indexOf(userInput.toLowerCase()) > -1
         );
-    }
-}
 
-export default Autocomplete;
+        setActiveOption(0);
+        setFilteredOptions(filteredOptions);
+        setShowOptions(true);
+        setUserInput(e.currentTarget.value);
+    };
+
+    let optionList;
+    if (showOptions && userInput)
+    {
+        if (filteredOptions.length)
+        {
+            optionList = (
+                <ul className="options">
+                    {filteredOptions.map((optionName, index) =>
+                    {
+                        let className = (index === activeOption) ? 'option-active' : '';
+                        return (
+                            <li className={className} key={optionName} onClick={onClick}>
+                                {optionName}
+                            </li>
+                        );
+                    })}
+                </ul>
+            );
+        }
+        else
+        {
+            optionList = (
+                <div className="no-options">
+                    <em>No Option!</em>
+                </div>
+            );
+        }
+    }
+    return (
+        <React.Fragment>
+            <div className="search">
+                <input
+                    type="text"
+                    className="search-box"
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}
+                    value={userInput}
+                />
+            </div>
+            {optionList}
+        </React.Fragment>
+    );
+};
+
+export default AutoSelect;
