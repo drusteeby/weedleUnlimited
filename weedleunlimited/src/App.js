93c7385d23244ac7182e3361d28ab77b34a3792e
drusteeby <drusteeby@gmail.com>
2023-06-08 20:52:06 -0400

    using a combobox all along styling changes

--- weedleunlimited/src/App.js
+++ weedleunlimited/src/App.js
@@ -4,8 +4,12 @@ import AutoSelect from './Components/AutoSelect';
 import pokedex from './Data/pokedex.json'
 import GuessTable from './Components/GuessTable';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import DropdownList from "react-widgets/Combobox";
+import "react-widgets/styles.css";
+import { Combobox } from 'react-widgets';
 
-function App() {
+function App()
+{
 
   let pokeNames = pokedex.map((entry) => entry.name.english);
   let correctAnswer = pokedex.find(entry => entry.name.english === "Weedle");
@@ -13,16 +17,38 @@ function App() {
   let onGuessSubmitted = (guess) => console.log(guess);
 
   return (
-<div className="App">
-    
-    <h1 className="App-header"> Weedle Unlimited</h1>
-    <h2 className="App-caption">WEEDLE GUESSING GAME</h2>
-    <GuessTable guesses={pokedex} correctAnswer={correctAnswer}/>
-    <AutoSelect
+    <div className="App">
+
+      <h1 className="App-header"> Weedle Unlimited</h1>
+      <h2 className="App-caption">WEEDLE GUESSING GAME</h2>
+      {/* <AutoSelect
         options={pokeNames}
         onGuessSubmitted={onGuessSubmitted}
-      />
-</div>
+      /> */}
+      <div className='justify-content-center'>
+        <div className='row justify-content-center'>
+          <div className='col-1' />
+          <div className='col-5'>
+            <Combobox data={pokeNames}
+              onSelect={onGuessSubmitted} />
+          </div>
+          <div className='col-1' />
+        </div>
+        <div className='row justify-content-center'>
+          <div className='col-1' />
+          <div className='col-5'>
+            <GuessTable guesses={pokedex} correctAnswer={correctAnswer} />
+          </div>
+          <div className='col-1' />
+        </div>
+
+      </div>
+
+
+
+
+
+    </div>
   );
 }
 
--- weedleunlimited/src/Components/GuessTable.jsx
+++ weedleunlimited/src/Components/GuessTable.jsx
@@ -4,19 +4,19 @@ const GuessTable = ({ guesses, correctAnswer }) =>
 {
     console.log(correctAnswer);
     return <>
-    <div className="d-flex flex-row justify-content-center mb-3">
-            <div className="col-md-2">Name</div>
-            <div className="col-md-2">Types(s)</div>
-            <div className="col-md-2">Base HP</div>
+    <div className="row justify-content-center mb-3">
+            <div className="col">Name</div>
+            <div className="col">Types(s)</div>
+            <div className="col">Base HP</div>
             </div>
         { guesses.map(guess => <div className="d-flex flex-row justify-content-center mb-3">
-            <div className="col-md-2 fw-bold">
+            <div className="col fw-bold">
                 <p className={correctAnswer.name === guess.name ? "text-success" : "text-dark"}> 
                     {guess.name.english}
                 </p>
             </div>
-            <div className="col-md-2">{guess.type.join(', ')}</div>
-            <div className="col-md-2">{guess.base.HP}</div>
+            <div className="col">{guess.type.join(', ')}</div>
+            <div className="col">{guess.base.HP}</div>
         </div>)}
     </>;
 };
