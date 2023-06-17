3b195a993edcbc6631f827412da449e0aa045480
drusteeby <drusteeby@gmail.com>
2023-06-16 20:05:06 -0400

    adding basic win/lose state logic

--- weedleunlimited/src/App.js
+++ weedleunlimited/src/App.js
@@ -13,29 +13,42 @@ function App()
 
   let correctAnswer = pokedex.find(entry => entry.name.english === "Weedle");
 
-  const [previousGusses, setPreviousGusses] = useState([]);
+  const [previousGuesses, setPreviousGuesses] = useState([]);
+  const [guessAttempts, setguessAttempts] = useState(0);
+  const [popupIsOpen, setpopupIsOpen] = useState(false);
+  const [maxGuessAttempts, setMaxGuessAttempts] = useState(8);
+
   let onGuessSubmitted = (guess) =>
   {
-    setPreviousGusses([...previousGusses, guess]);    
+    setPreviousGuesses([...previousGuesses, guess]);    
+    setguessAttempts(guessAttempts + 1);
+
+    if(guess.id == correctAnswer.id)
+    {
+      console.log("Congrats!")
+      return;
+    }
+
+    if(guessAttempts = maxGuessAttempts)
+    {
+      setpopupIsOpen(true);
+    }
   }
 
   return (
     <div className="App">
-      <Popup open={true} position="right center" modal>
+      <Popup open={popupIsOpen} position="right center" modal>
         <div>Popup content here !!</div>
       </Popup>
       <h1 className="App-header"> Weedle Unlimited</h1>
       <h2 className="App-caption">WEEDLE GUESSING GAME</h2>
-      {/* <AutoSelect
-        options={pokeNames}
-        onGuessSubmitted={onGuessSubmitted}
-      /> */}
+      <h3>{guessAttempts + ' of 8 guesses'}</h3>
       <div className='justify-content-center'>
         <div className='row justify-content-center'>
           <div className='col-1' />
           <div className='col-5'>
             <Combobox data={pokedex}
-              onSelect={onGuessSubmitted}
+              onSelect={onGuessSubmitted}            
               textField={(item) =>
               {
                 return typeof item === 'string' ? item : (item.name.english);
@@ -46,7 +59,7 @@ function App()
         <div className='row justify-content-center'>
           <div className='col-1' />
           <div className='col-5'>
-            <GuessTable guesses={previousGusses} correctAnswer={correctAnswer} />
+            <GuessTable guesses={previousGuesses} correctAnswer={correctAnswer} />
           </div>
           <div className='col-1' />
         </div>
