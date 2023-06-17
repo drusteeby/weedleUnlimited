da24e1b5113d5ea5f7baec0937e278977ad53dd7
drusteeby <drusteeby@gmail.com>
2023-06-17 00:52:38 -0400

    finished game loop logic

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { format } from 'react-string-format';
 function App()
 {
 
+
   let correctAnswer = pokedex.find(entry => entry.name.english === "Weedle");
   console.log(correctAnswer);
 
@@ -19,43 +20,71 @@ function App()
   const [guessAttempts, setguessAttempts] = useState(0);
   const [popupIsOpen, setpopupIsOpen] = useState(false);
   const [maxGuessAttempts, setMaxGuessAttempts] = useState(4);
+  const [popupContent, setPopupContent] = useState("");
+
+  let resetGame = () =>
+  {
+    setPreviousGuesses([]);
+    setguessAttempts(0);
+    setpopupIsOpen(false);
+  };
 
   let onGuessSubmitted = (guess) =>
   {
     let guessNumber = guessAttempts + 1;
 
-    setPreviousGuesses([...previousGuesses, guess]);    
+    setPreviousGuesses([...previousGuesses, guess]);
     setguessAttempts(guessNumber);
 
-    if(guess.id == correctAnswer.id)
+    if (guess.id == correctAnswer.id)
     {
-      console.log("Congrats!")
-      return;
+      setPopupContent(GeneratePopupContent(true));
     }
 
-    if(guessNumber >= maxGuessAttempts)
+    if (guessNumber >= maxGuessAttempts)
     {
-      setpopupIsOpen(true);
+      setPopupContent(GeneratePopupContent(false));
     }
+  }
+
+  let GeneratePopupContent = (winner) => 
+  {
+    setpopupIsOpen(true);
+    if (winner)
+    {
+      return <>
+        <div> You Win! </div>
+        <button onClick={resetGame}>
+          Reset Game
+        </button>
+      </>
+    }
+
+    return <>
+      <div>{format("The correct answer was {0}", correctAnswer.name.english)}</div>
+      <button onClick={resetGame}>
+        Reset Game
+      </button>
+    </>
   }
 
   return (
     <div className="App">
-      <Popup open={popupIsOpen} 
-             position="right center"
-             closeOnDocumentClick={false}
-             closeOnEscape={false}>
-        <div>Popup content here !!</div>
+      <Popup open={popupIsOpen}
+        position="right center"
+        closeOnDocumentClick={false}
+        closeOnEscape={false}>
+        {popupContent}
       </Popup>
       <h1 className="App-header"> Weedle Unlimited</h1>
       <h2 className="App-caption">WEEDLE GUESSING GAME</h2>
-      <h4>{format('{0} of {1} guesses', guessAttempts,maxGuessAttempts)}</h4>
+      <h4>{format('{0} of {1} guesses', guessAttempts, maxGuessAttempts)}</h4>
       <div className='justify-content-center'>
         <div className='row justify-content-center'>
           <div className='col-1' />
           <div className='col-5'>
             <Combobox data={pokedex}
-              onSelect={onGuessSubmitted}            
+              onSelect={onGuessSubmitted}
               textField={(item) =>
               {
                 return typeof item === 'string' ? item : (item.name.english);
@@ -76,4 +105,5 @@ function App()
 }
 
 export default App;
+
 
