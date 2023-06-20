3e30788dbc629a736a209875f0ba7d69a8f56bde
drusteeby <drusteeby@gmail.com>
2023-06-20 13:13:36 -0400

    stacking the types

--- src/Components/GuessTable.jsx
+++ src/Components/GuessTable.jsx
@@ -11,10 +11,8 @@ import StatBox from "./StatBox";
 
 const GuessTable = ({ guesses, correctAnswer }) =>
 {
-
-
     return <>
-        <div className="row justify-content-center mb-3" style={{backgroundColor: 'transparent'}}>
+        <div className="row justify-content-center mb-3" style={{ backgroundColor: 'transparent' }}>
             <div className="col">Name</div>
             <div className="col">Types(s)</div>
             <div className="col">HP</div>
@@ -24,27 +22,30 @@ const GuessTable = ({ guesses, correctAnswer }) =>
             <div className="col">Sp. Defense</div>
             <div className="col">Speed</div>
         </div>
-        {guesses.map(guess => <div key={guess.id} className="d-flex flex-row justify-content-center mb-3">
-            <div className="col fw-bold">
-                <p>
-                    {guess.name.english}
-                </p>
-            </div>
-            {guess.type.map(type =>
-                <>
-                    <div style={correctAnswer.type.some(t => t == type) ? { backgroundColor: 'green' } : {}}>
-                        {type}
-                    </div>
-                </>
-            )}
-            <StatBox guess={guess.base.HP} correctStat={correctAnswer.base.HP} />
-            <StatBox guess={guess.base.Attack} correctStat={correctAnswer.base.Attack} />
-            <StatBox guess={guess.base.Defense} correctStat={correctAnswer.base.Defense} />
-            <StatBox guess={guess.base.SpecialAttack} correctStat={correctAnswer.base.SpecialAttack} />
-            <StatBox guess={guess.base.SpecialDefense} correctStat={correctAnswer.base.SpecialDefense} />
-            <StatBox guess={guess.base.Speed} correctStat={correctAnswer.base.Speed} />
+        {guesses.map(guess =>
+            <div key={guess.id} className="d-flex flex-row justify-content-center mb-3">
+                <div className="col fw-bold">
+                    <p>
+                        {guess.name.english}
+                    </p>
+                </div>
+                <div className="col">
+                    {guess.type.map(type =>
+                        <>
+                            <div style={correctAnswer.type.some(t => t == type) ? { backgroundColor: 'green' } : {}}>
+                                {type}
+                            </div>
+                        </>
+                    )}
+                </div>
+                <StatBox guess={guess.base.HP} correctStat={correctAnswer.base.HP} />
+                <StatBox guess={guess.base.Attack} correctStat={correctAnswer.base.Attack} />
+                <StatBox guess={guess.base.Defense} correctStat={correctAnswer.base.Defense} />
+                <StatBox guess={guess.base.SpecialAttack} correctStat={correctAnswer.base.SpecialAttack} />
+                <StatBox guess={guess.base.SpecialDefense} correctStat={correctAnswer.base.SpecialDefense} />
+                <StatBox guess={guess.base.Speed} correctStat={correctAnswer.base.Speed} />
 
-        </div>)}
+            </div>)}
     </>;
 };
 
